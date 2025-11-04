5327ef19bb1851b69aff29a3d87538e2041eb773
Stephen <stephen.lu20@gmail.com>
2025-11-04 12:59:32 -0500

    Completed countTriple

--- java/src/main/java/io/zipcoder/StringsAndThings.java
+++ java/src/main/java/io/zipcoder/StringsAndThings.java
@@ -111,6 +111,16 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+        int count = 0;
+
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (i > 0 && i < input.length() - 1) {
+                char current = input.charAt(i);
+                if (input.charAt(i-1) == current && input.charAt(i+1) == current) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
