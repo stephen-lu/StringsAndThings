47c1f3403e54165730af6891948720a10b39d847
Stephen <stephen.lu20@gmail.com>
2025-11-06 16:35:44 -0500

    Simplified gIsHappy method

--- java/src/main/java/io/zipcoder/StringsAndThings.java
+++ java/src/main/java/io/zipcoder/StringsAndThings.java
@@ -82,24 +82,7 @@ public class StringsAndThings {
      *           gHappy("xxggyygxx") // Should return  false
      */
     public Boolean gIsHappy(String input){
-        Boolean happy = true;
-
-        for (int i = 0; i < input.length() - 1; i++) {
-            if (i > 0 && i < input.length() - 1 && input.charAt(i) == 'g') {
-                if (input.charAt(i-1) != 'g' && input.charAt(i+1) != 'g') {
-                    happy = false;
-                }
-            } else if (i == 0 && input.charAt(i) == 'g') {
-                if (input.charAt(i+1) != 'g') {
-                        happy = false;
-                }
-            } else if (i == input.length() - 1 && input.charAt(i) == 'g') {
-                if (input.charAt(i-1) != 'g') {
-                        happy = false;
-                }
-            }
-        }
-        return happy;
+        return input.contains("gg");
     }
 
 
